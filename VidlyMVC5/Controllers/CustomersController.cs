1b989d52ef19c6abe20d4a28c9d9145392e2f692
JohnnyPeng <johnnypeng_2003@hotmail.com>
2018-02-22 16:56:19 +0800

    Section 2 Exercise

---
+++ VidlyMVC5/Controllers/CustomersController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+using Microsoft.Ajax.Utilities;
+using VidlyMVC5.Models;
+
+namespace VidlyMVC5.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult CustomerList()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        // Get Customer Detail
+        public ActionResult CustomerDetail(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var customer = GetCustomers().SingleOrDefault( c => c.Id == id );
+
+            if (customer == null)
+                return HttpNotFound();
+           
+            return View(customer);
+        }
+
+        private static IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer() { Id = 1, Name = "Johnny"},
+                new Customer() { Id = 2, Name = "Jerry"}
+            };
+        }
+    }
+}
+
+
---
+++ VidlyMVC5/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyMVC5.Models;
+
+namespace VidlyMVC5.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult MovieList()
+        {
+            var movies = new List<Movie> ()
+            {
+                new Movie() { Name = "Shark" },
+                new Movie() { Name = "StarWar" }
+            };
+            return View(movies);
+        }
+    }
+}
