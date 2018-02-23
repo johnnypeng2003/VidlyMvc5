16245b8dd5b820a88e5ca94f1bc0cbe5faecdb37
JohnnyPeng <johnnypeng_2003@hotmail.com>
2018-02-23 15:21:16 +0800

    第35講  Exercise 3: Display the list of movies and their details

--- VidlyMVC5/Controllers/MoviesController.cs
+++ VidlyMVC5/Controllers/MoviesController.cs
@@ -1,23 +1,37 @@
 ﻿using System;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Serialization;
 using VidlyMVC5.Models;
 
 namespace VidlyMVC5.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext(); 
+        }    
+
         // GET: Movies
         public ActionResult MovieList()
         {
-            var movies = new List<Movie> ()
-            {
-                new Movie() { Name = "Shark" },
-                new Movie() { Name = "StarWar" }
-            };
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+
             return View(movies);
+        }
+
+        public ActionResult MovieDetail(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            return View(movie);
+
         }
     }
 }
